e50f0f81595afde8b5f7e28d88c1d764a7b66783
Mansour <mansour@oxplot.com>
2012-10-15 23:53:37 +1100

    CropPhotoAcitivity UI is mostly done
    Signed-off-by: Mansour <mansour@oxplot.com>

--- src/com/oxplot/contactphotosync/CropPhotoActivity.java
+++ src/com/oxplot/contactphotosync/CropPhotoActivity.java
@@ -1,27 +1,113 @@
 package com.oxplot.contactphotosync;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class CropPhotoActivity extends Activity {
 
-  File thumbTemp;
-  Bitmap thumb;
+  private boolean loaded = false;
+  private ProgressBar loadingBar;
+  private CropView cropView;
+  private PrepareTask preparer;
+  private File inCachePath;
+  private int orientation;
 
   @Override
   public void onCreate(Bundle savedState) {
     super.onCreate(savedState);
     setContentView(R.layout.activity_crop_photo);
+    loadingBar = (ProgressBar) findViewById(R.id.loading);
+    cropView = (CropView) findViewById(R.id.cropView);
 
-    // Make a copy of the source file
-    // Determine the orientation of the image using ExifInterface
-    // Create a thumbnail for drawing on the screen
-    // Setup CropView
+    if (savedState != null && savedState.containsKey("loaded"))
+      loaded = true;
+
+    if (loaded) {
+      RectF bound = new RectF();
+      cropView.setEnforceRatio(savedState.getBoolean("enforce_ratio"));
+      cropView.setCropWRatio(savedState.getFloat("wratio"));
+      cropView.setCropHRatio(savedState.getFloat("hratio"));
+      cropView.setThumbnail((Bitmap) savedState.getParcelable("thumbnail"));
+      cropView.setOrgWidth(savedState.getInt("width"));
+      cropView.setOrgHeight(savedState.getInt("height"));
+      orientation = savedState.getInt("orientation");
+      bound.left = savedState.getFloat("crop_left");
+      bound.top = savedState.getFloat("crop_top");
+      bound.right = savedState.getFloat("crop_right");
+      bound.bottom = savedState.getFloat("crop_bottom");
+      cropView.setCropBound(bound);
+      inCachePath = new File(savedState.getString("cache_path"));
+      initAfterLoad();
+    } else {
+      preparer = new PrepareTask();
+      preparer.execute();
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    if (preparer != null)
+      preparer.cancel(true);
+    super.onDestroy();
+  }
+
+  private void goodbye(int result) {
+    if (inCachePath != null)
+      inCachePath.delete();
+    setResult(result);
+    finish();
+  }
+
+  private void initAfterLoad() {
+    loadingBar.setVisibility(View.GONE);
+    cropView.setVisibility(View.VISIBLE);
+  }
+
+  @Override
+  public void onBackPressed() {
+    goodbye(RESULT_CANCELED);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    if (!loaded)
+      return;
+    outState.putBoolean("loaded", true);
+    outState.putParcelable("thumbnail", cropView.getThumbnail());
+    outState.putInt("width", cropView.getOrgWidth());
+    outState.putInt("height", cropView.getOrgHeight());
+    outState.putInt("orientation", orientation);
+    outState.putString("cache_path", inCachePath.getAbsolutePath());
+    RectF bound = cropView.getCropBound();
+    outState.putFloat("crop_left", bound.left);
+    outState.putFloat("crop_right", bound.right);
+    outState.putFloat("crop_top", bound.top);
+    outState.putFloat("crop_bottom", bound.bottom);
+    outState.putFloat("wratio", cropView.getCropWRatio());
+    outState.putFloat("hratio", cropView.getCropHRatio());
+    outState.putBoolean("enforce_ratio", cropView.isEnforceRatio());
   }
 
   @Override
@@ -33,13 +119,187 @@ public class CropPhotoActivity extends Activity {
   public boolean onMenuItemSelected(int featureId, MenuItem item) {
     switch (item.getItemId()) {
     case R.id.menu_crop:
+      // TODO
       break;
     case R.id.menu_cancel_crop:
-      setResult(RESULT_CANCELED);
-      finish();
+      goodbye(RESULT_CANCELED);
       break;
     }
     return true;
   }
 
+  private class PrepareTask extends AsyncTask<Void, Void, Integer> {
+
+    private static final int OK = 0;
+    private static final int FAILED = 1;
+
+    private Uri src;
+    private Uri dst;
+    private File inCachePath;
+    private Bitmap thumbnail;
+    private int orgWidth, orgHeight, orientation;
+
+    public PrepareTask() {
+      src = getIntent().getData();
+      cropView.setVisibility(View.GONE);
+      loadingBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected Integer doInBackground(Void... params) {
+      AssetFileDescriptor fdin = null, fdout = null;
+      InputStream is = null;
+      OutputStream os = null;
+      byte[] buffer = new byte[4096];
+      int bytesRead;
+      Bitmap interm = null;
+
+      try {
+
+        // Make a copy of the source content in local cache
+
+        inCachePath = File.createTempFile("cropper-", "", getCacheDir());
+        dst = Uri.fromFile(inCachePath);
+
+        fdin = getContentResolver().openAssetFileDescriptor(src, "r");
+        is = fdin.createInputStream();
+        fdout = getContentResolver().openAssetFileDescriptor(dst, "w");
+        os = fdout.createOutputStream();
+
+        bytesRead = is.read(buffer);
+        while (bytesRead >= 0) {
+          if (isCancelled())
+            return FAILED;
+          os.write(buffer, 0, bytesRead);
+          bytesRead = is.read(buffer);
+        }
+
+        is.close();
+        os.close();
+        fdin.close();
+        fdout.close();
+
+        // Retrieve primary information about the image
+
+        Options opts = new Options();
+        opts.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(inCachePath.getAbsolutePath(), opts);
+        if (opts.outWidth < 0 || opts.outHeight < 0)
+          return FAILED;
+        orgWidth = opts.outWidth;
+        orgHeight = opts.outHeight;
+
+        // For JPEG files, read the orientation details
+
+        orientation = 0;
+        if ("image/jpeg".equals(opts.outMimeType)) {
+          ExifInterface exif = new ExifInterface(inCachePath.getAbsolutePath());
+          switch (exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1)) {
+          case 3:
+            orientation = 180;
+            break;
+          case 6:
+            orientation = 90;
+            break;
+          case 8:
+            orientation = 270;
+            break;
+          }
+        }
+
+        // Create a thumbnail for drawing on the screen
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point scr = new Point();
+        display.getSize(scr);
+
+        // Determine a suitable thumbnail dimension
+
+        int orgMaxDim = Math.max(orgWidth, orgHeight);
+        int orgMinDim = Math.min(orgWidth, orgHeight);
+        int scrMaxDim = Math.max(scr.x, scr.y);
+        int scrMinDim = Math.min(scr.x, scr.y);
+        double scale = 1;
+        if (orgMaxDim > scrMaxDim)
+          scale = ((double) scrMaxDim) / orgMaxDim;
+        if (scale * orgMinDim > scrMinDim)
+          scale = ((double) scrMinDim) / orgMinDim;
+        int thumbWidth = (int) Math.round(scale * orgWidth);
+        int thumbHeight = (int) Math.round(scale * orgHeight);
+
+        opts.inJustDecodeBounds = false;
+        opts.inSampleSize = orgWidth / thumbWidth;
+        interm = BitmapFactory.decodeFile(inCachePath.getAbsolutePath(), opts);
+        if (isCancelled())
+          return FAILED;
+        Matrix m = new Matrix();
+        m.postRotate(orientation);
+        m.postScale(((float) thumbWidth) / opts.outWidth, ((float) thumbHeight)
+            / opts.outHeight);
+        thumbnail = Bitmap.createBitmap(interm, 0, 0, opts.outWidth,
+            opts.outHeight, m, true);
+
+        if (orientation == 90 || orientation == 270) {
+          int t = orgWidth;
+          orgWidth = orgHeight;
+          orgHeight = t;
+        }
+
+        return OK;
+
+      } catch (IOException e) {
+        return FAILED;
+      } finally {
+        if (os != null)
+          try {
+            os.close();
+          } catch (IOException e) {}
+        if (is != null)
+          try {
+            is.close();
+          } catch (IOException e) {}
+        if (fdin != null)
+          try {
+            fdin.close();
+          } catch (IOException e) {}
+        if (fdout != null)
+          try {
+            fdout.close();
+          } catch (IOException e) {}
+      }
+    }
+
+    @Override
+    protected void onCancelled() {
+    }
+
+    @Override
+    protected void onPostExecute(Integer result) {
+      if (result == OK) {
+        cropView.setEnforceRatio(true);
+        cropView.setCropHRatio(1);
+        cropView.setCropWRatio(1);
+        cropView.setThumbnail(thumbnail);
+        CropPhotoActivity.this.orientation = orientation;
+        cropView.setOrgWidth(orgWidth);
+        cropView.setOrgHeight(orgHeight);
+        CropPhotoActivity.this.inCachePath = inCachePath;
+        cropView.setThumbnail(thumbnail);
+        RectF bound = new RectF();
+        bound.left = 0;
+        bound.top = 0;
+        bound.right = orgWidth;
+        bound.bottom = orgHeight;
+        cropView.setCropBound(bound);
+        loaded = true;
+        initAfterLoad();
+      } else {
+        Toast.makeText(CropPhotoActivity.this,
+            getResources().getString(R.string.something_went_wrong),
+            Toast.LENGTH_LONG).show();
+        goodbye(RESULT_CANCELED);
+      }
+    }
+
+  }
 }
--- src/com/oxplot/contactphotosync/CropView.java
+++ src/com/oxplot/contactphotosync/CropView.java
@@ -2,10 +2,57 @@ package com.oxplot.contactphotosync;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class CropView extends View {
+
+  private static final int PADDING = 20;
+  private static final int HANDLE_SIZE = 20;
+  private static final int ACTIVE_TOUCH_RADIUS = 40;
+  private static final int MIN_SCREEN_DIM = 50;
+  private static final int CORNER_TL = 0;
+  private static final int CORNER_TR = 1;
+  private static final int CORNER_BL = 2;
+  private static final int CORNER_BR = 3;
+  private static final int CORNER_MID = 5;
+
+  private Bitmap thumbnail;
+  private float cropWRatio;
+  private float cropHRatio;
+  private int orgWidth;
+  private int orgHeight;
+  private RectF bound;
+  private RectF transBound;
+  private RectF transFullBound;
+  private float[] tmpPoint;
+  private Matrix thumbMatrix;
+  private Matrix orgMatrix;
+  private Matrix orgMatrixInverse;
+  private Paint thumbPaint;
+  private Paint handlePaint;
+  private Paint framePaint;
+  private Paint shadePaint;
+  private Path shadePath;
+  private Path shadePathIn;
+  private boolean dragging;
+  private int draggedCorner;
+  private PointF dragOffset;
+  private Path handlePath;
+  private float minCropW;
+  private float minCropH;
+  private boolean enforceRatio;
 
   public CropView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -16,6 +63,321 @@ public class CropView extends View {
     } finally {
       a.recycle();
     }
+    setBackgroundColor(Color.BLACK);
+    orgMatrixInverse = new Matrix();
+    orgMatrix = new Matrix();
+    thumbMatrix = new Matrix();
+    thumbPaint = new Paint();
+    thumbPaint.setFilterBitmap(true);
+    bound = new RectF();
+    transBound = new RectF();
+    tmpPoint = new float[2];
+    transFullBound = new RectF();
+    handlePaint = new Paint();
+    handlePaint.setAntiAlias(true);
+    handlePaint.setColor(Color.rgb(0, 204, 255));
+    handlePaint.setStyle(Style.FILL);
+    framePaint = new Paint();
+    framePaint.setColor(Color.rgb(0, 204, 255));
+    framePaint.setStyle(Style.STROKE);
+    framePaint.setStrokeWidth(2);
+    framePaint.setPathEffect(new DashPathEffect(new float[] { 5, 5 }, 0));
+    shadePaint = new Paint();
+    shadePaint.setColor(Color.argb(140, 0, 0, 0));
+    shadePaint.setStyle(Style.FILL);
+    shadePath = new Path();
+    shadePathIn = new Path();
+    dragging = false;
+    dragOffset = new PointF();
+    handlePath = new Path();
+    handlePath.moveTo(-HANDLE_SIZE, 0);
+    handlePath.lineTo(0, HANDLE_SIZE);
+    handlePath.lineTo(HANDLE_SIZE, 0);
+    handlePath.lineTo(0, -HANDLE_SIZE);
+    handlePath.lineTo(-HANDLE_SIZE, 0);
+  }
+
+  public boolean isEnforceRatio() {
+    return enforceRatio;
+  }
+
+  public void setEnforceRatio(boolean enforceRatio) {
+    this.enforceRatio = enforceRatio;
+    invalidateDims(getWidth(), getHeight());
+  }
+
+  public float getCropWRatio() {
+    return cropWRatio;
+  }
+
+  public void setCropWRatio(float cropWRatio) {
+    this.cropWRatio = cropWRatio;
+  }
+
+  public float getCropHRatio() {
+    return cropHRatio;
+  }
+
+  public void setCropHRatio(float cropHRatio) {
+    this.cropHRatio = cropHRatio;
+  }
+
+  private double getDist(double x1, double y1, double x2, double y2) {
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  }
+
+  private void fixBound(int corner) {
+
+    if (bound.left < 0) {
+      if (corner == CORNER_MID)
+        bound.right -= bound.left;
+      bound.left = 0;
+    }
+    if (bound.top < 0) {
+      if (corner == CORNER_MID)
+        bound.bottom -= bound.top;
+      bound.top = 0;
+    }
+    if (bound.right > orgWidth) {
+      if (corner == CORNER_MID)
+        bound.left += orgWidth - bound.right;
+      bound.right = orgWidth;
+    }
+    if (bound.bottom > orgHeight) {
+      if (corner == CORNER_MID)
+        bound.top += orgHeight - bound.bottom;
+      bound.bottom = orgHeight;
+    }
+
+    if (corner != CORNER_MID) {
+      float fw = bound.right - bound.left;
+      float fh = bound.bottom - bound.top;
+      fw = fw < minCropW ? minCropW : fw;
+      fh = fh < minCropH ? minCropH : fh;
+
+      if ((fw * cropHRatio) / cropWRatio <= fh)
+        fh = (fw * cropHRatio) / cropWRatio;
+      else
+        fw = (fh * cropWRatio) / cropHRatio;
+
+      if (corner == CORNER_BR || corner == CORNER_TR)
+        bound.right = bound.left + fw;
+      else if (corner == CORNER_BL || corner == CORNER_TL)
+        bound.left = bound.right - fw;
+
+      if (corner == CORNER_TL || corner == CORNER_TR)
+        bound.top = bound.bottom - fh;
+      else if (corner == CORNER_BL || corner == CORNER_BR)
+        bound.bottom = bound.top + fh;
+    }
+
+    transBound.left = bound.left;
+    transBound.right = bound.right;
+    transBound.top = bound.top;
+    transBound.bottom = bound.bottom;
+    orgMatrix.mapRect(transBound);
+
+    shadePathIn.reset();
+    shadePathIn.addRect(transBound, Path.Direction.CW);
+    shadePath.reset();
+    shadePath.addRect(transFullBound, Path.Direction.CW);
+    shadePath.addPath(shadePathIn);
+    shadePath.setFillType(Path.FillType.EVEN_ODD);
+
+  }
+
+  public void setCropBound(RectF bound) {
+    this.bound.left = bound.left;
+    this.bound.right = bound.right;
+    this.bound.bottom = bound.bottom;
+    this.bound.top = bound.top;
+    fixBound(CORNER_BR);
+    postInvalidate();
+  }
+
+  public RectF getCropBound() {
+    return new RectF(bound);
+  }
+
+  public void setThumbnail(Bitmap thumbnail) {
+    this.thumbnail = thumbnail;
+    invalidateDims(getWidth(), getHeight());
+  }
+
+  public Bitmap getThumbnail() {
+    return thumbnail;
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    canvas.save();
+
+    canvas.concat(thumbMatrix);
+    canvas.drawBitmap(thumbnail, 0, 0, thumbPaint);
+
+    canvas.restore();
+
+    canvas.drawPath(shadePath, shadePaint);
+    canvas.drawRect(transBound, framePaint);
+
+    if (!dragging) {
+
+      canvas.save();
+      canvas.translate(transBound.left, transBound.top);
+      canvas.drawPath(handlePath, handlePaint);
+      canvas.restore();
+
+      canvas.save();
+      canvas.translate(transBound.left, transBound.bottom);
+      canvas.drawPath(handlePath, handlePaint);
+      canvas.restore();
+
+      canvas.save();
+      canvas.translate(transBound.right, transBound.top);
+      canvas.drawPath(handlePath, handlePaint);
+      canvas.restore();
+
+      canvas.save();
+      canvas.translate(transBound.right, transBound.bottom);
+      canvas.drawPath(handlePath, handlePaint);
+      canvas.restore();
+    }
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    float x = event.getX(), y = event.getY();
+    if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+      float cx = 0, cy = 0;
+      draggedCorner = -1;
+      if (getDist(x, y, transBound.left, transBound.top) < ACTIVE_TOUCH_RADIUS) {
+        draggedCorner = CORNER_TL;
+        cx = transBound.left;
+        cy = transBound.top;
+      } else if (getDist(x, y, transBound.right, transBound.top) < ACTIVE_TOUCH_RADIUS) {
+        draggedCorner = CORNER_TR;
+        cx = transBound.right;
+        cy = transBound.top;
+      } else if (getDist(x, y, transBound.left, transBound.bottom) < ACTIVE_TOUCH_RADIUS) {
+        draggedCorner = CORNER_BL;
+        cx = transBound.left;
+        cy = transBound.bottom;
+      } else if (getDist(x, y, transBound.right, transBound.bottom) < ACTIVE_TOUCH_RADIUS) {
+        draggedCorner = CORNER_BR;
+        cx = transBound.right;
+        cy = transBound.bottom;
+      } else if (x >= transBound.left && x <= transBound.right
+          && y >= transBound.top && y <= transBound.bottom) {
+        draggedCorner = CORNER_MID;
+        cx = transBound.left;
+        cy = transBound.top;
+      }
+      if (draggedCorner != -1) {
+        dragOffset.x = cx - x;
+        dragOffset.y = cy - y;
+        dragging = true;
+        invalidate();
+      }
+    } else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+      dragging = false;
+      invalidate();
+    } else if (dragging && event.getActionMasked() == MotionEvent.ACTION_MOVE) {
+      tmpPoint[0] = x + dragOffset.x;
+      tmpPoint[1] = y + dragOffset.y;
+      orgMatrixInverse.mapPoints(tmpPoint);
+      if (draggedCorner == CORNER_TL) {
+        bound.left = tmpPoint[0];
+        bound.top = tmpPoint[1];
+      } else if (draggedCorner == CORNER_TR) {
+        bound.right = tmpPoint[0];
+        bound.top = tmpPoint[1];
+      } else if (draggedCorner == CORNER_BL) {
+        bound.left = tmpPoint[0];
+        bound.bottom = tmpPoint[1];
+      } else if (draggedCorner == CORNER_BR) {
+        bound.right = tmpPoint[0];
+        bound.bottom = tmpPoint[1];
+      } else if (draggedCorner == CORNER_MID) {
+        float w = bound.right - bound.left;
+        float h = bound.bottom - bound.top;
+        bound.left = tmpPoint[0];
+        bound.top = tmpPoint[1];
+        bound.right = bound.left + w;
+        bound.bottom = bound.top + h;
+      }
+      fixBound(draggedCorner);
+      invalidate();
+    }
+    return true;
+  }
+
+  public int getOrgWidth() {
+    return orgWidth;
+  }
+
+  public void setOrgWidth(int orgWidth) {
+    this.orgWidth = orgWidth;
+    invalidateDims(getWidth(), getHeight());
+  }
+
+  public int getOrgHeight() {
+    return orgHeight;
+  }
+
+  public void setOrgHeight(int orgHeight) {
+    this.orgHeight = orgHeight;
+    invalidateDims(getWidth(), getHeight());
+  }
+
+  private void invalidateDims(int w, int h) {
+
+    if (thumbnail == null)
+      return;
+
+    int fw = w - PADDING * 2;
+    int fh = h - PADDING * 2;
+    float scale = 1;
+    scale = (float) fw / thumbnail.getWidth();
+    if (scale * thumbnail.getHeight() > fh)
+      scale = (float) fh / thumbnail.getHeight();
+    thumbMatrix.reset();
+    thumbMatrix.preTranslate(
+        (float) fw / 2 - (float) (scale * thumbnail.getWidth()) / 2 + PADDING,
+        (float) fh / 2 - (float) (scale * thumbnail.getHeight()) / 2 + PADDING);
+    thumbMatrix.preScale(scale, scale);
+
+    scale = scale * thumbnail.getWidth() / orgWidth;
+    orgMatrix.reset();
+    orgMatrix.preTranslate((float) fw / 2 - (float) (scale * orgWidth) / 2
+        + PADDING, (float) fh / 2 - (float) (scale * orgHeight) / 2 + PADDING);
+    orgMatrix.preScale(scale, scale);
+    orgMatrix.invert(orgMatrixInverse);
+
+    transFullBound.left = 0;
+    transFullBound.right = w;
+    transFullBound.top = 0;
+    transFullBound.bottom = h;
+
+    if (enforceRatio) {
+      if (cropWRatio > cropHRatio) {
+        minCropH = MIN_SCREEN_DIM / scale;
+        minCropW = (minCropH * cropWRatio) / cropHRatio;
+      } else {
+        minCropW = MIN_SCREEN_DIM / scale;
+        minCropH = (minCropW * cropHRatio) / cropWRatio;
+      }
+    } else {
+      minCropH = minCropW = MIN_SCREEN_DIM / scale;
+    }
+
+    fixBound(CORNER_BR);
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    invalidateDims(w, h);
   }
 
 }
